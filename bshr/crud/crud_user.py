1533446b5f12c528b24476125e3985a1852feb79
avo888 <avoevodin8888@gmail.com>
2022-12-18 19:55:48 +0300

    added pwd context util added user create method in crud

--- bshr/crud/crud_user.py
+++ bshr/crud/crud_user.py
@@ -4,7 +4,9 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import Session
 
 from crud.base import CRUDBase
-from schemas import User, UserCreate, UserUpdate
+from models import User
+from schemas import UserCreate, UserUpdate
+from utils.password import password_hash_ctx
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
@@ -19,3 +21,22 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         res = await db.execute(select(self.model).filter())
         found_user = res.scalar_one_or_none()
         return found_user
+
+    async def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        """
+        Create a user.
+
+        :param db: SQLAlchemy session
+        :param obj_in: user create schema
+        :return:  user model
+        """
+        user_db = User(
+            username=obj_in.username,
+            email=obj_in.email,
+            password=password_hash_ctx.hash(obj_in.password),
+        )
+
+        db.add(user_db)
+        await db.commit()
+        await db.refresh(user_db)
+        return user_db
---
+++ bshr/utils/password.py
@@ -0,0 +1,16 @@
+"""
+Password utils.
+
+Attrs:
+    password_hash_ctx: context for creating password using
+                       password based key derivative func 2
+                       algorithm.
+"""
+
+from passlib.context import CryptContext
+
+password_hash_ctx = CryptContext(
+    schemes=["pbkdf2_sha256"],
+    pbkdf2_sha256__min_rounds=18000,
+    pbkdf2_sha256__max_rounds=26000,
+)
