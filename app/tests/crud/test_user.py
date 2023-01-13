bedcbd0f2b4243d23ad87c1acfe3163dd26681dc
avo888 <avoevodin8888@gmail.com>
2023-01-13 23:42:44 +0300

    updated init files for tests

---
+++ app/tests/crud/test_user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+
+
+def test_create_user(db: Session) -> None:
+    """
+    Test create user crud operation
+
+    Args:
+        db: SQLAlchemy session
+
+    Returns:
+
+    """
+    pass
