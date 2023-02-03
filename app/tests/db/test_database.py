414ef89f798b4184d7919ec293bd9030807873c5
avo888 <avoevodin8888@gmail.com>
2023-02-03 16:55:54 +0300

    added database init without first superuser email test

---
+++ app/tests/db/test_database.py
@@ -0,0 +1,26 @@
+import importlib
+import os
+from unittest import mock
+
+import pytest
+from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+@pytest.mark.asyncio
+async def test_init_db_without_first_user_email(
+    settings_env_dict_function_scope,
+) -> None:
+    settings_env_dict_function_scope["FIRST_SUPERUSER_EMAIL"] = ""
+    with mock.patch.dict(os.environ, settings_env_dict_function_scope):
+        import app.db.database
+        from app.core.config import Settings
+
+        settings = Settings()
+
+        with mock.patch.object(app.db.database, "settings", settings):
+            from app.db.database import app_init_db
+
+            app = FastAPI()
+            await app_init_db(app)
+            assert isinstance(app.state.db, AsyncSession)
