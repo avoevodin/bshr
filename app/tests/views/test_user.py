a6a0ca2fd3a2a49ac636ebd48b76008e3053b577
avo888 <avoevodin8888@gmail.com>
2023-03-03 19:18:56 +0300

    updated user register test

--- app/tests/views/test_user.py
+++ app/tests/views/test_user.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from app import schemas, crud
+from app.core.security import verify_password
 from app.tests.utils.utils import random_lower_string, random_email
 
 
@@ -27,3 +28,4 @@ async def test_user_register_username_success(
     user_db = await crud.user.get_by_email(db, email=user_data.email)
     assert user_db.email == user_data.email
     assert user_db.username == user_data.username
+    assert verify_password(user_data.password, user_db.password)
