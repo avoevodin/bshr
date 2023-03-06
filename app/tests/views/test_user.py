02514f8cff3e59827dd1f9a5c16ffcf0e52b2fcf
avo888 <avoevodin8888@gmail.com>
2023-03-06 20:58:21 +0300

    added user register view test with email

--- app/tests/views/test_user.py
+++ app/tests/views/test_user.py
@@ -52,3 +52,26 @@ async def test_user_register_username_success(
     assert user_db.email == user_data.email
     assert user_db.username == user_data.username
     assert verify_password(user_data.password, user_db.password)
+
+
+@pytest.mark.asyncio
+async def test_user_register_email_success(
+    db: AsyncSession,
+    get_client: AsyncClient,
+    get_app: FastAPI,
+) -> None:
+    password = random_lower_string(8)
+    user_data = schemas.UserCreate(
+        username=random_lower_string(8),
+        email=random_email(),
+        password=password,
+    )
+    response = await get_client.post(
+        get_app.url_path_for("users:register"), content=user_data.json()
+    )
+    assert response.status_code == status.HTTP_200_OK
+    user_db = await crud.user.get_by_username(db, username=user_data.username)
+    assert user_db
+    assert user_db.email == user_data.email
+    assert user_db.username == user_data.username
+    assert verify_password(user_data.password, user_db.password)
