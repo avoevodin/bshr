01d813db9036007c622edb499d7cdf3c9f72ad01
avo888 <avoevodin8888@gmail.com>
2023-03-17 11:48:23 +0300

    added user update view test user not found

--- app/tests/views/test_user.py
+++ app/tests/views/test_user.py
@@ -211,3 +211,35 @@ async def test_update_unauthorized(
     response = await get_client.patch(get_app.url_path_for("users:update", user_id=-1))
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
     assert "Not authenticated" in response.content.decode()
+
+
+@pytest.mark.asyncio
+async def test_update_not_found(
+    db: AsyncSession,
+    get_client: AsyncClient,
+    get_app: FastAPI,
+    settings_with_test_env: BaseSettings,
+) -> None:
+    user_data = schemas.UserCreate(
+        username=settings_with_test_env.FIRST_SUPERUSER,
+        email=settings_with_test_env.FIRST_SUPERUSER_EMAIL,
+        password=settings_with_test_env.FIRST_SUPERUSER_PASSWORD,
+    )
+    response = await get_client.post(
+        get_app.url_path_for("auth:token"),
+        data={
+            "username": user_data.username,
+            "password": settings_with_test_env.FIRST_SUPERUSER_PASSWORD,
+        },
+        headers={"content-type": "application/x-www-form-urlencoded"},
+    )
+    user_update_data = schemas.UserUpdate(email="some_new@email.com")
+    user_id = -1
+    token = response.json()
+    response = await get_client.patch(
+        get_app.url_path_for("users:update", user_id=user_id),
+        headers={"Authorization": f"Bearer {token.get('access_token')}"},
+        content=user_update_data.json(),
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert f"User with id <{user_id}> not found" in response.content.decode()
