8a99b9bd1ba864a75ddd576aadde744cff5b1274
avo888 <avoevodin8888@gmail.com>
2023-03-25 13:18:53 +0800

    updated test user update view with current user credentials

--- app/tests/views/test_user.py
+++ app/tests/views/test_user.py
@@ -348,3 +348,5 @@ async def test_update_success_current_user(
         content=user_update_data.json(),
     )
     assert response.status_code == status.HTTP_200_OK
+    user_updated_data = schemas.User.parse_obj(json.loads(response.content.decode()))
+    assert user_id == user_updated_data.id
